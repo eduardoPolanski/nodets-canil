08e7e8520dab5a95b9b906b3dc2e36aba1d4492a
Eduardo <eduardo_polanski@hotmail.com>
2021-08-16 18:43:39 -0300

    base criada

---
+++ src/controllers/pageController.ts
@@ -0,0 +1,26 @@
+import { Request, Response } from 'express';
+import { send } from 'process';
+
+export const home = (req: Request, res: Response) => {
+    
+    // res.render('pages/pages');
+    
+}
+
+export const dogs = (req: Request, res: Response) => {
+    
+     // res.render('pages/dogs');
+    
+}
+
+export const cats = (req: Request, res: Response) => {
+    
+     // res.render('pages/cats');
+    
+}
+
+export const fishes = (req: Request, res: Response) => {
+    
+     // res.render('pages/fishes');
+    
+}
